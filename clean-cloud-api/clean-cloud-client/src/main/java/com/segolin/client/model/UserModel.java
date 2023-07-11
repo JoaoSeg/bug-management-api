987e4e7e06e221aaa3cf5209e20910c36c6ea12b
JoaoSegolin <joe-ws@hotmail.com>
2023-07-11 15:30:56 -0300

    Refactor fields first and last name because of mispelling

--- clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/model/UserModel.java
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/model/UserModel.java
@@ -9,8 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserModel {
 
-    private String firstname;
-    private String lastname;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     //TODO Create logic to check if passwords match
