5e51a87e16e082050f88f33f10fb9d6ced831f74
JoaoSegolin <joe-ws@hotmail.com>
2023-07-11 20:41:56 -0300

    Add interface method for token resending

--- clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/service/UserService.java
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/service/UserService.java
@@ -1,10 +1,15 @@
 package com.segolin.client.service;
 
 import com.segolin.client.entity.User;
+import com.segolin.client.entity.VerificationToken;
 import com.segolin.client.model.UserModel;
 
 public interface UserService {
     User registerUser(UserModel userModel);
 
     void saveVerificationTokenForUser(String token, User user);
+
+    String validateVerificationToken(String token);
+
+    VerificationToken generateNewVerificationToken(String oldToken);
 }
