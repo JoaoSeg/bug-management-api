643f586442fb105481ce872615514d46211ee506
JoaoSegolin <joe-ws@hotmail.com>
2023-07-11 20:42:44 -0300

    Implement interface method for user token revalidation on expiring

--- clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/service/UserServiceImpl.java
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/service/UserServiceImpl.java
@@ -7,9 +7,13 @@ import com.segolin.client.model.UserModel;
 import com.segolin.client.repository.UserRepository;
 import com.segolin.client.repository.VerificationTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.cache.SpringCacheBasedUserCache;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -28,8 +32,8 @@ public class UserServiceImpl implements UserService{
     public User registerUser(UserModel userModel) {
         User user = new User();
         user.setEmail(userModel.getEmail());
-        user.setFirstname(userModel.getFirstname());
-        user.setLastname(userModel.getLastname());
+        user.setFirstName(userModel.getFirstName());
+        user.setLastName(userModel.getLastName());
         user.setRole("USER");
         user.setPassword(passwordEncoder.encode(userModel.getPassword()));
 
@@ -42,5 +46,34 @@ public class UserServiceImpl implements UserService{
         VerificationToken verificationToken = new VerificationToken(user, token);
         verificationTokenRepository.save(verificationToken);
 
+    }
+
+    @Override
+    public String validateVerificationToken(String token) {
+        VerificationToken verificationToken = verificationTokenRepository.findByToken(token);
+
+        if (verificationToken == null) {
+            return "invalid";
+        }
+
+        User user = verificationToken.getUser();
+        Calendar calendar = Calendar.getInstance();
+
+        if ((verificationToken.getExpirationTime().getTime() - calendar.getTime().getTime() <= 0))  {
+            verificationTokenRepository.delete(verificationToken);
+            return "expired";
+        }
+
+        user.setEnabled(true);
+        userRepository.save(user);
+        return "valid";
+    }
+
+    @Override
+    public VerificationToken generateNewVerificationToken(String oldToken) {
+        VerificationToken verificationToken = verificationTokenRepository.findByToken(oldToken);
+        verificationToken.setToken(UUID.randomUUID().toString());
+        verificationTokenRepository.save(verificationToken);
+        return verificationToken;
     }
 }
