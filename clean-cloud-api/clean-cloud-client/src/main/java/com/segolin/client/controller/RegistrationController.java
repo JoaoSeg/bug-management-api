bf1a161e919a0831a0e2a83d97d0c2789e840a6f
JoaoSegolin <joe-ws@hotmail.com>
2023-07-11 08:12:02 -0300

    Add method to publish account verification link

--- clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/controller/RegistrationController.java
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/controller/RegistrationController.java
@@ -1,9 +1,12 @@
 package com.segolin.client.controller;
 
 import com.segolin.client.entity.User;
+import com.segolin.client.event.RegistrationCompleteEvent;
 import com.segolin.client.model.UserModel;
 import com.segolin.client.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +17,22 @@ public class RegistrationController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+
     @PostMapping("/register")
-    public String registerUser(@RequestBody UserModel userModel) {
+    public String registerUser(@RequestBody UserModel userModel, final HttpServletRequest request) {
         User user = userService.registerUser(userModel);
+        publisher.publishEvent(new RegistrationCompleteEvent(
+                user,
+                applicationUrl(request)
+        ));
         return "Success";
+    }
+
+    private String applicationUrl(HttpServletRequest request) {
+        return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
     }
 
 }
