e63f5881911732897311a8c34a88920719392782
JoaoSegolin <joe-ws@hotmail.com>
2023-07-11 08:09:56 -0300

    Create VerificationToken repo

---
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/repository/VerificationTokenRepository.java
@@ -0,0 +1,7 @@
+package com.segolin.client.repository;
+
+import com.segolin.client.entity.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+}
