660506f60e9c5ce544371162f1ca5909a6ee2f59
JoaoSegolin <joe-ws@hotmail.com>
2023-07-11 08:08:43 -0300

    Create event listener and event for user registration

---
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/event/listener/RegistrationCompleteEventListener.java
@@ -0,0 +1,29 @@
+package com.segolin.client.event.listener;
+
+import com.segolin.client.entity.User;
+import com.segolin.client.event.RegistrationCompleteEvent;
+import com.segolin.client.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+
+import java.util.UUID;
+
+@Slf4j
+public class RegistrationCompleteEventListener implements ApplicationListener<RegistrationCompleteEvent> {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void onApplicationEvent(RegistrationCompleteEvent event) {
+        //TODO Create the Verification Token for the User with link
+        User user = event.getUser();
+        String token = UUID.randomUUID().toString();
+        userService.saveVerificationTokenForUser(token, user);
+        //TODO Send mail to user
+        String url = event.getApplicationUrl() + "verifyRegistration?token=" + token;
+        //sendVerificationEmail()
+        log.info("Click the link to verify your account: {}", url);
+    }
+}
