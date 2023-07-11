cd4f1089496344f1bc0e71899dbcad77a1f5ec4a
JoaoSegolin <joe-ws@hotmail.com>
2023-07-11 16:00:50 -0300

    Add custom method findbytoken to repo

--- clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/repository/VerificationTokenRepository.java
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/repository/VerificationTokenRepository.java
@@ -4,4 +4,5 @@ import com.segolin.client.entity.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    VerificationToken findByToken(String token);
 }
