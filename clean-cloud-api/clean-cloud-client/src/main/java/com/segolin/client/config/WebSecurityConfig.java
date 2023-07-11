bab61adb7f28835838838c583075e99fd29a2f2c
JoaoSegolin <joe-ws@hotmail.com>
2023-07-11 16:02:30 -0300

    Fix filter chain using depreciated functions

--- clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/config/WebSecurityConfig.java
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/config/WebSecurityConfig.java
@@ -1,9 +1,13 @@
 package com.segolin.client.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,7 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class WebSecurityConfig {
 
-    private static final String[] WHITE_LIST_URLS = {"/hello", "/register"};
+    private static final String[] WHITE_LIST_URLS = {"/hello", "/register", "/", "/verifyRegistration"};
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -21,9 +25,17 @@ public class WebSecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.
-                authorizeHttpRequests((requests) -> requests
-                        .requestMatchers(WHITE_LIST_URLS).permitAll());
+        http
+                .csrf().disable()
+                .cors().disable()
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers(WHITE_LIST_URLS).permitAll()
+                        .anyRequest().authenticated())
+                .exceptionHandling()
+                .accessDeniedHandler((request, response, ex) -> {
+                    System.out.println("Access denied" + ex.getMessage());
+                    response.sendError(HttpServletResponse.SC_FORBIDDEN);
+                });
 
         return http.build();
     }
