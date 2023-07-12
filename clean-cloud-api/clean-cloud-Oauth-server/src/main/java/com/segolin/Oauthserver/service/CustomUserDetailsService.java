6e1325f65051432dea7fb2392f53bf0dcb572a78
JoaoSegolin <joe-ws@hotmail.com>
2023-07-12 15:59:50 -0300

    Change passEncoder Bean declaration to config file

--- clean-cloud-api/clean-cloud-Oauth-server/src/main/java/com/segolin/Oauthserver/service/CustomUserDetailsService.java
+++ clean-cloud-api/clean-cloud-Oauth-server/src/main/java/com/segolin/Oauthserver/service/CustomUserDetailsService.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -21,6 +22,9 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
