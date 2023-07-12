8af9503038d29e5db73cf2e5fa5cd654f83db6ae
JoaoSegolin <joe-ws@hotmail.com>
2023-07-12 09:06:43 -0300

    Begin flow pass reset and save, create endpoints and pass reset link

--- clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/controller/RegistrationController.java
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/controller/RegistrationController.java
@@ -3,14 +3,17 @@ package com.segolin.client.controller;
 import com.segolin.client.entity.User;
 import com.segolin.client.entity.VerificationToken;
 import com.segolin.client.event.RegistrationCompleteEvent;
+import com.segolin.client.model.PasswordModel;
 import com.segolin.client.model.UserModel;
 import com.segolin.client.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @Slf4j
@@ -48,6 +51,41 @@ public class RegistrationController {
         User user = verificationToken.getUser();
         resendVerificationToken(user, applicationUrl(request), verificationToken);
         return "Verification link sent";
+    }
+
+    @PostMapping("/resetPassword")
+    public String resetPassword(@RequestBody PasswordModel passwordModel, HttpServletRequest request) {
+        User user = userService.findUserByEmail(passwordModel.getEmail());
+        String url = "";
+        if (user != null) {
+            String token = UUID.randomUUID().toString();
+            userService.createPasswordResetTokenForUser(user, token);
+            url = passwordResetTokenEmail(user, applicationUrl(request), token);
+        }
+        return url;
+    }
+
+    @PostMapping("/savePassword")
+    public String savePassword(@RequestParam("token") String token, @RequestBody PasswordModel passwordModel) {
+        String result = userService.validatePasswordResetToken(token);
+        if (!result.equalsIgnoreCase("valid")) {
+            return "Invalid Token";
+        }
+
+        Optional<User> user = userService.getUserByPasswordResetToken(token);
+        if (user.isPresent()) {
+            userService.changePassword(user.get(), passwordModel.getNewPassword());
+            return "Password Reset Successfully";
+        } else {
+            return "Invalid Token";
+        }
+
+    }
+
+    private String passwordResetTokenEmail(User user, String applicationUrl, String token) {
+        String url = applicationUrl + "/savePassword?token=" + token;
+        log.info("Click the link to reset your password: {}", url);
+        return url;
     }
 
     private void resendVerificationToken(User user, String applicationUrl, VerificationToken verificationToken) {
