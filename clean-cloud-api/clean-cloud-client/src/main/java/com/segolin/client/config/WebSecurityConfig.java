9701dd87a4e1a007b13950613fd94370f479968c
JoaoSegolin <joe-ws@hotmail.com>
2023-07-12 20:07:56 -0300

    Fix deprecated methods using lambda calls

--- clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/config/WebSecurityConfig.java
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/config/WebSecurityConfig.java
@@ -4,9 +4,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,10 +20,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     private static final String[] WHITE_LIST_URLS = {
-            "/hello", "/register", "/",
-            "/verifyRegistration*", "/resendVerifyToken*",
-            "/resetPassword", "/savePassword",
-            "/changePassword"
+            "/hello",
+            "/register",
+            "/verifyRegistration*",
+            "/resendVerifyToken"
     };
 
     @Bean
@@ -31,16 +34,15 @@ public class WebSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .cors().disable()
-                .authorizeHttpRequests((request) -> request
+                .authorizeHttpRequests((request)  -> request
                         .requestMatchers(WHITE_LIST_URLS).permitAll()
-                        .anyRequest().authenticated())
-                .exceptionHandling()
-                .accessDeniedHandler((request, response, ex) -> {
-                    System.out.println("Access denied" + ex.getMessage());
-                    response.sendError(HttpServletResponse.SC_FORBIDDEN);
-                });
+                        .requestMatchers("/api/**").authenticated()
+                )
+                .oauth2Login((oauth2 -> oauth2
+                        .loginPage("/oauth2/authorization/api-client-oidc")))
+                .oauth2Client(Customizer.withDefaults())
+                .cors(CorsConfigurer::disable)
+                .csrf(CsrfConfigurer::disable);
 
         return http.build();
     }
