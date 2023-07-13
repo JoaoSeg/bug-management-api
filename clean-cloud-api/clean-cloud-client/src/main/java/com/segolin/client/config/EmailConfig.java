af092c9e37d4b1869208fe45787aeabe5bec55a0
JoaoSeg <64279044+JoaoSeg@users.noreply.github.com>
2023-07-13 16:00:24 -0300

    Create email config to handle email sending

---
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/config/EmailConfig.java
@@ -0,0 +1,33 @@
+package com.segolin.client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender()
+    {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(25);
+        //TODO Hide credentials on config file
+        mailSender.setUsername("joaosegolin@gmail.com");
+        mailSender.setPassword("eocbwatiqdvgganc");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+
+}
