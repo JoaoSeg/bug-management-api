4a88956134b12db0a68a32baf55196d98c62a0c9
JoaoSeg <64279044+JoaoSeg@users.noreply.github.com>
2023-07-13 16:52:52 -0300

    Move custom service to client

---
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/service/CustomEmployeeDetailsService.java
@@ -0,0 +1,54 @@
+package com.segolin.client.service;
+
+import com.segolin.client.entity.Employee;
+import com.segolin.client.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomEmployeeDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Employee employee = userRepository.findByEmail(email);
+        if (employee == null) {
+            throw new UsernameNotFoundException("No user found");
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                employee.getEmail(),
+                employee.getPassword(),
+                employee.isEnabled(),
+                true,
+                true,
+                true,
+                getAuthorities(List.of(employee.getRole()))
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(List<String> roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for(String role: roles) {
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+        return authorities;
+    }
+}
