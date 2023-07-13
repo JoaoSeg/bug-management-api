857d0cb19cdb31b60f92e60ade8c658f5d0c40cd
JoaoSeg <64279044+JoaoSeg@users.noreply.github.com>
2023-07-13 16:58:14 -0300

    Update methods adress

--- clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/controller/HelloController.java
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/controller/HelloController.java
@@ -1,12 +1,30 @@
 package com.segolin.client.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.security.Principal;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.*;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/api/hello")
-    public String hello() { return "Hello, Welcome to this page."; }
+    @GetMapping("/")
+    public String hello(Principal principal) {
+        return "Hello, home";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Hello, secured";
+    }
+
+
 
 }
