184143e04806fbb657228ee4b1b03905e87d1558
JoaoSeg <64279044+JoaoSeg@users.noreply.github.com>
2023-07-14 11:53:58 -0300

    Add authority for roles endpoints
    Create role specific static strings to avoid repetition and add auth validation for specific roles endpoints in security filter

--- clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/config/WebSecurityConfig.java
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/config/WebSecurityConfig.java
@@ -24,6 +24,10 @@ import static org.springframework.security.config.Customizer.*;
 @Configuration
 public class WebSecurityConfig {
 
+    private static final String TESTER = "/api/bug/tester";
+    private static final String DEVELOPER = "/api/bug/developer";
+    private static final String MANAGER = "/api/bug/manager";
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(11);
@@ -35,6 +39,13 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests((request)  -> {
                     request.requestMatchers("/").permitAll();
                     request.requestMatchers(HttpMethod.POST, "/register").permitAll();
+                    request.requestMatchers(HttpMethod.POST, TESTER).hasAnyAuthority("TESTER");
+                    request.requestMatchers(HttpMethod.PUT, TESTER).hasAnyAuthority("TESTER");
+                    request.requestMatchers(HttpMethod.DELETE, TESTER).hasAnyAuthority("TESTER");
+                    request.requestMatchers(HttpMethod.GET, DEVELOPER).hasAnyAuthority("DEVELOPER");
+                    request.requestMatchers(HttpMethod.GET, "/api/bug/developer/listbugs").hasAnyAuthority("DEVELOPER");
+                    request.requestMatchers(HttpMethod.PUT, DEVELOPER).hasAnyAuthority("DEVELOPER");
+                    request.requestMatchers(HttpMethod.GET, MANAGER).hasAnyAuthority("MANAGER");
                     request.requestMatchers(HttpMethod.GET, "/verifyRegistration*").permitAll();
                     request.requestMatchers(HttpMethod.GET, "/secured/user").hasAnyAuthority("USER");
                     request.anyRequest().authenticated();
