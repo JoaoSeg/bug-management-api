b97a341f786f87d98c866e3d7bcb7d1f22d2b4f2
JoaoSeg <64279044+JoaoSeg@users.noreply.github.com>
2023-07-13 21:57:58 -0300

    Add list by status method to repository

--- clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/repository/BugRepository.java
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/repository/BugRepository.java
@@ -3,5 +3,8 @@ package com.segolin.client.repository;
 import com.segolin.client.entity.Bug;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BugRepository extends JpaRepository<Bug, Long> {
+    List<Bug> findAllByStatus(String reported);
 }
