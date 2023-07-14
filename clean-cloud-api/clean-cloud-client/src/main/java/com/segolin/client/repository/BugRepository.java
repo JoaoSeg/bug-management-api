094900c61ec3a6231be125db26fd779b98d1f7ea
JoaoSeg <64279044+JoaoSeg@users.noreply.github.com>
2023-07-14 11:56:17 -0300

    Add repo query to generate manager dash

--- clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/repository/BugRepository.java
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/repository/BugRepository.java
@@ -3,8 +3,11 @@ package com.segolin.client.repository;
 import com.segolin.client.entity.Bug;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface BugRepository extends JpaRepository<Bug, Long> {
     List<Bug> findAllByStatus(String reported);
+
+    List<Bug> findAllByStatusAndCreation(String status, Timestamp period);
 }
