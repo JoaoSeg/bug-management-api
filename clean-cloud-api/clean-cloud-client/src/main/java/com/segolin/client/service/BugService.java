316f53f202ba4cbcdd7c17a05e52ddcc347018af
JoaoSeg <64279044+JoaoSeg@users.noreply.github.com>
2023-07-13 22:02:10 -0300

    Add controller methods to service

--- clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/service/BugService.java
+++ clean-cloud-api/clean-cloud-client/src/main/java/com/segolin/client/service/BugService.java
@@ -1,4 +1,24 @@
 package com.segolin.client.service;
 
+import com.segolin.client.entity.Bug;
+import com.segolin.client.model.BugModel;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+
+@Service
 public interface BugService {
+    Bug registerBug(BugModel bugModel);
+
+    void updateBug(BugModel bugModel, Long id);
+
+    void deleteBug(Long id);
+
+    String listReportedBugs();
+
+    String listBug(Long id);
+
+    String alterBugStatus(Long id, String status);
+
+    String generateDash(String status, Timestamp period);
 }
